bb816dd851c5f7c0c3a732d31850fff351cc498a
minaatefmaf <minaatefmaf@gmail.com>
2015-08-31 10:02:12 +0200

    Organize the project files.

--- On The Map/LoginViewController.swift
+++ On The Map/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  On The Map
 //
 //  Created by Mina Atef on 8/30/15.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
