9951b941849d0fb926997b93b65dc0ef80dfcefe
minaatefmaf <minaatefmaf@gmail.com>
2015-09-07 06:53:38 +0200

    Implement the table tab.

--- On The Map/StudentLocationsTableViewController.swift
+++ On The Map/StudentLocationsTableViewController.swift
@@ -13,11 +13,21 @@ class StudentLocationsTableViewController: UIViewController {
     // Varaibles to hold the user data & unique key
     var userData: UdacityUser!
     var uniqueKey: String!
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         // Populate the userData & uniqueKey with the data from the login scene
         userData = (UIApplication.sharedApplication().delegate as! AppDelegate).udacityUserData
         uniqueKey = (UIApplication.sharedApplication().delegate as! AppDelegate).userUniqueID
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        
+        // Reload the rows and sections of the table view.
+        tableView.reloadData()
     }
     
     @IBAction func logoutButton(sender: UIBarButtonItem) {
@@ -31,4 +41,27 @@ class StudentLocationsTableViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.appDelegate.studentsLocations!.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BasicTableCell") as! UITableViewCell
+        
+        // Set the name and the image
+        let student = self.appDelegate.studentsLocations![indexPath.row]
+        cell.textLabel?.text = "\(student.firstName) \(student.lastName)"
+       // cell.imageView?.image = UIImage(named: "pin")
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Open the media url in safari
+        let student = self.appDelegate.studentsLocations![indexPath.row]
+        if let requestUrl = NSURL(string: student.mediaURL) {
+            UIApplication.sharedApplication().openURL(requestUrl)
+        }
+    }
+
 }
