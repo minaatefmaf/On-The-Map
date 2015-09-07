bb06bc8d59e1d3a5d76800175d7cec20e0129466
minaatefmaf <minaatefmaf@gmail.com>
2015-09-07 07:34:43 +0200

    Implement the collection tab.

---
+++ On The Map/MapStringCollectionCell.swift
@@ -0,0 +1,15 @@
+//
+//  MapStringCollectionCell.swift
+//  On The Map
+//
+//  Created by Mina Atef on 9/7/15.
+//  Copyright (c) 2015 minaatefmaf. All rights reserved.
+//
+
+import UIKit
+
+class MapStringCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var mapStringLabel: UILabel!
+    
+}
--- On The Map/StudentLocationsCollectionViewController.swift
+++ On The Map/StudentLocationsCollectionViewController.swift
@@ -13,11 +13,21 @@ class StudentLocationsCollectionViewController: UIViewController {
     // Varaibles to hold the user data & unique key
     var userData: UdacityUser!
     var uniqueKey: String!
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         // Populate the userData & uniqueKey with the data from the login scene
         userData = (UIApplication.sharedApplication().delegate as! AppDelegate).udacityUserData
         uniqueKey = (UIApplication.sharedApplication().delegate as! AppDelegate).userUniqueID
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        
+        // Reload the rows and sections of the collection view.
+        collectionView.reloadData()
     }
     
     @IBAction func logoutButton(sender: UIBarButtonItem) {
@@ -29,6 +39,30 @@ class StudentLocationsCollectionViewController: UIViewController {
         
         // Dismiss the view controller
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.appDelegate.studentsLocations!.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MapStringCollectionCell", forIndexPath: indexPath) as! MapStringCollectionCell
+        let student = self.appDelegate.studentsLocations![indexPath.row]
+        
+        // Set the map string
+        cell.mapStringLabel.text = student.mapString
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath:NSIndexPath)
+    {
+        // Open the media url in safari
+        let student = self.appDelegate.studentsLocations![indexPath.row]
+        if let requestUrl = NSURL(string: student.mediaURL) {
+            UIApplication.sharedApplication().openURL(requestUrl)
+        }
     }
     
 }
